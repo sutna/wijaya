76a12fcb4e04aa7639d8c79b212ae30f03f8a257
Sutna Wijaya <54989490+sutna@users.noreply.github.com>
2019-09-14 20:44:36 +0700

    20190801365_luassegitiga.js
    3. buat program menghitung luas segi tiga

---
+++ luassegitiga.js
@@ -0,0 +1,8 @@
+var masukankeyboard = require("readline-sync");
+
+var atas = parseInt(masukankeyboard.question("masukan atas"));
+var tinggi = parseInt(masukankeyboard.question("masukan tinggi"));
+
+var setengahluas = alas*tinggi*2;
+
+console.log("luas segitiga ="+setengahluas);
